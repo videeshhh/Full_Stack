34710e9a9ae2b9c7a2313050fd0ec5821e5a3035
Videshhh <sharmavideeesh@gmail.com>
2026-07-04 13:50:53 +0530

    QR-code

--- qr/index.js
+++ qr/index.js
@@ -1,11 +1,20 @@
 import inquirer from 'inquirer';
+import qr from "qr-image";
+import fs from "fs";
 
 inquirer
   .prompt([
-    /* Pass your questions in here */
+    {"message" : "Type your url: ", name : "URL"}
   ])
   .then((answers) => {
-    // Use user feedback for... whatever!!
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr-image.png"));
+    
+    fs.writeFile("URL.txt" , url ,(err)=>{
+        if(err) throw err;
+        console.log("URL is saved in URL.txt");
+    });
   })
   .catch((error) => {
     if (error.isTtyError) {
@@ -14,3 +23,6 @@ inquirer
       // Something else went wrong
     }
   });
+
+
+
