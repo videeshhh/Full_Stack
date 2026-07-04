98839c4dd58e18c6d2794f507406afbf1a5d94b3
Videshhh <sharmavideeesh@gmail.com>
2026-07-04 11:45:19 +0530

    learn npm

---
+++ npm/index.js
@@ -0,0 +1,8 @@
+import  {randomSuperhero} from "superheroes";
+const name = randomSuperhero();
+console.log(name);
+
+import generateName from "sillyname";
+
+var sillyName = generateName();
+console.log(sillyName);
