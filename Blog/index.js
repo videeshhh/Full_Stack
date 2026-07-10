0a16c7249639fab8091f161499a6778442cfe144
Videshhh <sharmavideeesh@gmail.com>
2026-07-10 15:35:29 +0530

    Finished project

--- Blog/index.js
+++ Blog/index.js
@@ -4,24 +4,32 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 const blogs = [];
+let nextId = 1;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-app.get("/" , (req,res)=>{
-    res.render("index.ejs");
+app.get("/" , (req, res)=>{
+    const isSuccess = req.query.success === 'true';
+    res.render("index.ejs", { showSuccessMessage: isSuccess })
 });
 
-app.post("/submit" , (res,req)=>{
-        const blog = {
+app.post("/submit" , (req, res)=>{
+    blogs.push({
+        id : nextId++,
         title: req.body.title,
         text: req.body.text
-    };
-    blogs.push(blog);
-    res.redirect("/");
+    });
+    res.render("success.ejs");
+        
+}); 
+
+app.get("/blogs" , (req, res)=>{
+    res.render("blogs.ejs", { blogs });
 });
 
 app.listen(port , ()=>{
     console.log(`Server started on ${port}`);
 });
+
