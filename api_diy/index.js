a69e0ffb19b6470570fb1dd5cc8eb3d37ac77ae5
Videshhh <sharmavideeesh@gmail.com>
2026-07-23 12:21:41 +0530

    updating api

--- api_diy/index.js
+++ api_diy/index.js
@@ -6,14 +6,37 @@ const port = 3000;
 const masterKey = "4VGP2DN-6EWM4SJ-N6FGRHV-Z3PR3TT";
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
-//1. GET a random joke
+app.get("/random" , (req , res)=>{
+  const randomIndex = Math.floor(Math.random()*jokes.length);
+  res.json(jokes[randomIndex]);
+});
 
-//2. GET a specific joke
+app.get("/jokes/:id" ,(req , res)=>{
+  const id = parseInt(req.params.id);
+  const foundJoke = jokes.find((joke)=> joke.id===id);
+  res.json(foundJoke);
+});
 
-//3. GET a jokes by filtering on the joke type
+app.get("/filter" ,(req , res)=>{
+  const type = req.query.type;
+  const foundJoke = jokes.filter((joke)=> joke.jokeType===type);
+  console.log(foundJoke);
+  res.json(foundJoke);
+});
 
-//4. POST a new joke
+
+app.post("/jokes" , (req , res)=>{
+  const newJoke = {
+    id : jokes.length + 1,
+    jokeText : req.body.text,
+    jokeType : req.body.type,
+  };
+  jokes.push(newJoke);
+  console.log(newJoke);
+  res.json(newJoke);
+})
 
 //5. PUT a joke
 
