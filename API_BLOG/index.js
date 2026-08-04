a788d2eb3928a1603ba43040b8058a932d550bd7
Videshhh <sharmavideeesh@gmail.com>
2026-08-04 18:16:58 +0530

    Fixed typos and added patch functionality ; ,

--- API_BLOG/index.js
+++ API_BLOG/index.js
@@ -62,13 +62,30 @@ app.post("/posts" , (req , res)=>{
     title : req.body.title,
     content : req.body.content,
     author : req.body.author,
-    date : new Date();
+    date : new Date(),
   }
+  lastId = newId;
+  posts.push(post);
+  res.status(201).json(post);
 });
 
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
+app.patch("/posts/:id" , (req , res)=>{
+  const id = parseInt(req.params.id);
+  const post = posts.find((p)=> p.id === id);
+  if(!post) return res.status(404).json({message : "Post is not present"});
+
+  if(req.body.title) post.title = req.body.title;
+  if(req.body.author) post.author = req.body.author;
+  if(req.body.content) post.content = req.body.content;
+  res.json(post);
+});
+
 
 //CHALLENGE 5: DELETE a specific post by providing the post id.
+app.delete("/posts/delete/:id" , (res , req)=>{
+  
+});
 
 app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}`);
