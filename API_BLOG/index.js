d5850f50130290d6cce602e47f3db49ee7402bd0
Videshhh <sharmavideeesh@gmail.com>
2026-08-04 18:20:28 +0530

    Added delete functionality

--- API_BLOG/index.js
+++ API_BLOG/index.js
@@ -84,7 +84,11 @@ app.patch("/posts/:id" , (req , res)=>{
 
 //CHALLENGE 5: DELETE a specific post by providing the post id.
 app.delete("/posts/delete/:id" , (res , req)=>{
-  
+  const id = parseInt(req.params.id);
+  const post = posts.find((p)=> p.id === id);
+  if(post === -1) return res.status(404).json({message : "post not found"});
+  posts.splice(post , 1);
+  res.json({message : "post is successfully deleted"});
 });
 
 app.listen(port, () => {
