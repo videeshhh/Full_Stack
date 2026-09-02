7ecd1eefdb0b3237fee0debc0de5268985f2c0cb
Videshhh <sharmavideeesh@gmail.com>
2026-09-02 22:54:26 +0530

    React props

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./../public/styles.css";
-import App from "./app.jsx";
+import App from "./App.jsx";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <App />
+    <App />
 );
