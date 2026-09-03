f1748d94357a7cb11421f50cfec7bc76dc2e28cc
Videshhh <sharmavideeesh@gmail.com>
2026-09-04 00:29:23 +0530

    React export props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,28 +2,20 @@ import React from "react";
 import contacts from "./contacts";
 import Card from "./card";
 
+function createCard(contact){
+    return <Card 
+                name={contact.name}/*here contact.name says that take name from contacts and save it under name using contact.name*/
+                img={contact.imgURL}
+                phone={contact.phone}
+                email={contact.email}
+            />                
+}
+
 function App(){
     return (
         <div>
-            <h1 className="heading">My Contacts</h1>
-            <Card 
-                name={contacts[0].name}
-                img={contacts[0].imgURL}
-                phone={contacts[0].phone}
-                email={contacts[0].email}
-            />
-            <Card 
-                name={contacts[1].name}
-                img={contacts[1].imgURL}
-                phone={contacts[1].phone}
-                email={contacts[1].email}
-            />
-            <Card 
-                name={contacts[2].name}
-                img={contacts[2].imgURL}
-                phone={contacts[2].phone}
-                email={contacts[2].email}
-            />
+            <h1 className="heading">My contacts</h1>
+            {contacts.map(createCard)}
         </div>
     )
 }
